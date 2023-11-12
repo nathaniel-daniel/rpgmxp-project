a0c1d6dd28d0390c32de04a612d2cadbf6e04a10
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2023-11-11 18:39:39 -0800

    Add MoveCommand type

--- rpgmxp-types/src/lib.rs
+++ rpgmxp-types/src/lib.rs
@@ -3,6 +3,7 @@ mod event_command;
 mod event_command_parameter;
 mod event_page_condition;
 mod event_page_graphic;
+mod move_command;
 mod script;
 
 pub use self::audio_file::AudioFile;
@@ -10,4 +11,5 @@ pub use self::event_command::EventCommand;
 pub use self::event_command_parameter::EventCommandParameter;
 pub use self::event_page_condition::EventPageCondition;
 pub use self::event_page_graphic::EventPageGraphic;
+pub use self::move_command::MoveCommand;
 pub use self::script::Script;
---
+++ rpgmxp-types/src/move_command.rs
@@ -0,0 +1,104 @@
+use crate::EventCommandParameter;
+use ruby_marshal::FromValue;
+use ruby_marshal::FromValueError;
+use ruby_marshal::IntoValue;
+use ruby_marshal::IntoValueError;
+use ruby_marshal::ObjectValue;
+use ruby_marshal::ValueArena;
+use ruby_marshal::ValueHandle;
+use std::collections::HashSet;
+
+const OBJECT_NAME: &[u8] = b"RPG::MoveCommand";
+
+const PARAMETERS_FIELD: &[u8] = b"@parameters";
+const CODE_FIELD: &[u8] = b"@code";
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct MoveCommand {
+    pub parameters: Vec<EventCommandParameter>,
+    pub code: i32,
+}
+
+impl<'a> FromValue<'a> for MoveCommand {
+    fn from_value(
+        arena: &'a ValueArena,
+        handle: ValueHandle,
+        visited: &mut HashSet<ValueHandle>,
+    ) -> Result<Self, FromValueError> {
+        let object: &ObjectValue = FromValue::from_value(arena, handle, visited)?;
+
+        let name = object.name();
+        let name = arena
+            .get_symbol(name)
+            .ok_or(FromValueError::InvalidValueHandle {
+                handle: name.into(),
+            })?
+            .value();
+        if name != OBJECT_NAME {
+            return Err(FromValueError::UnexpectedObjectName { name: name.into() });
+        }
+
+        let instance_variables = object.instance_variables();
+
+        let mut parameters_field = None;
+        let mut code_field = None;
+
+        for (key, value) in instance_variables.iter().copied() {
+            let key = arena
+                .get_symbol(key)
+                .ok_or_else(|| FromValueError::InvalidValueHandle { handle: key.into() })?
+                .value();
+
+            match key {
+                PARAMETERS_FIELD => {
+                    if parameters_field.is_some() {
+                        return Err(FromValueError::DuplicateInstanceVariable { name: key.into() });
+                    }
+
+                    parameters_field = Some(FromValue::from_value(arena, value, visited)?);
+                }
+                CODE_FIELD => {
+                    if code_field.is_some() {
+                        return Err(FromValueError::DuplicateInstanceVariable { name: key.into() });
+                    }
+
+                    code_field = Some(FromValue::from_value(arena, value, visited)?);
+                }
+                _ => {
+                    return Err(FromValueError::UnknownInstanceVariable { name: key.into() });
+                }
+            }
+        }
+
+        let parameters =
+            parameters_field.ok_or_else(|| FromValueError::MissingInstanceVariable {
+                name: PARAMETERS_FIELD.into(),
+            })?;
+        let code = code_field.ok_or_else(|| FromValueError::MissingInstanceVariable {
+            name: CODE_FIELD.into(),
+        })?;
+
+        Ok(Self { parameters, code })
+    }
+}
+
+impl IntoValue for MoveCommand {
+    fn into_value(self, arena: &mut ValueArena) -> Result<ValueHandle, IntoValueError> {
+        let object_name = arena.create_symbol(OBJECT_NAME.into());
+
+        let parameters_field_key = arena.create_symbol(PARAMETERS_FIELD.into());
+        let code_field_key = arena.create_symbol(CODE_FIELD.into());
+
+        let parameters_field_value = self.parameters.into_value(arena)?;
+        let code_field_value = self.code.into_value(arena)?;
+
+        let fields = vec![
+            (parameters_field_key, parameters_field_value),
+            (code_field_key, code_field_value),
+        ];
+
+        let object = arena.create_object(object_name, fields);
+
+        Ok(object.into())
+    }
+}
