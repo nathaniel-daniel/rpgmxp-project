4a65c678a7d935a4794efc47916c755f17ba36de
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2024-02-11 21:18:51 -0800

    Add more map fields

--- rpgmxp-project/src/commands/init/map.rs
+++ rpgmxp-project/src/commands/init/map.rs
@@ -1,5 +1,6 @@
 use rpgmxp_types::AudioFile;
 use rpgmxp_types::Event;
+use ruby_marshal::ArrayValue;
 use ruby_marshal::FromValue;
 use ruby_marshal::FromValueError;
 use ruby_marshal::HashValue;
@@ -27,6 +28,14 @@ pub struct Map {
     pub bgm: AudioFile,
     pub tileset_id: i32,
     pub events: Vec<(i32, Event)>,
+    pub bgs: AudioFile,
+    pub autoplay_bgm: bool,
+    // pub data: RpgTable,
+    pub autoplay_bgs: bool,
+    pub height: i32,
+    pub encounter_step: i32,
+    pub width: i32,
+    pub encounter_list: Vec<()>,
 }
 
 impl<'a> FromValue<'a> for Map {
@@ -53,14 +62,14 @@ impl<'a> FromValue<'a> for Map {
         let mut bgm_field = None;
         let mut tileset_id_field = None;
         let mut events_field = None;
-        // let mut bgs_field = None;
-        // let mut autoplay_bgm_field = None;
+        let mut bgs_field = None;
+        let mut autoplay_bgm_field = None;
         // let mut data_field = None;
-        // let mut autoplay_bgs_field = None;
-        // let mut height_field = None;
-        // let mut encounter_step_field = None;
-        // let mut width_field = None;
-        //let mut encounter_list_field = None;
+        let mut autoplay_bgs_field = None;
+        let mut height_field = None;
+        let mut encounter_step_field = None;
+        let mut width_field = None;
+        let mut encounter_list_field = None;
 
         for (key, value) in instance_variables.iter() {
             let key = arena
@@ -103,6 +112,65 @@ impl<'a> FromValue<'a> for Map {
 
                     events_field = Some(new_events);
                 }
+                BGS_FIELD => {
+                    if bgs_field.is_some() {
+                        return Err(FromValueError::DuplicateInstanceVariable { name: key.into() });
+                    }
+
+                    bgs_field = Some(FromValue::from_value(arena, *value, visited)?);
+                }
+                AUTOPLAY_BGM_FIELD => {
+                    if autoplay_bgm_field.is_some() {
+                        return Err(FromValueError::DuplicateInstanceVariable { name: key.into() });
+                    }
+
+                    autoplay_bgm_field = Some(FromValue::from_value(arena, *value, visited)?);
+                }
+                DATA_FIELD => {
+                    // todo!("DATA")
+                }
+                AUTOPLAY_BGS_FIELD => {
+                    if autoplay_bgs_field.is_some() {
+                        return Err(FromValueError::DuplicateInstanceVariable { name: key.into() });
+                    }
+
+                    autoplay_bgs_field = Some(FromValue::from_value(arena, *value, visited)?);
+                }
+                HEIGHT_FIELD => {
+                    if height_field.is_some() {
+                        return Err(FromValueError::DuplicateInstanceVariable { name: key.into() });
+                    }
+
+                    height_field = Some(FromValue::from_value(arena, *value, visited)?);
+                }
+                ENCOUNTER_STEP_FIELD => {
+                    if encounter_step_field.is_some() {
+                        return Err(FromValueError::DuplicateInstanceVariable { name: key.into() });
+                    }
+
+                    encounter_step_field = Some(FromValue::from_value(arena, *value, visited)?);
+                }
+                WIDTH_FIELD => {
+                    if width_field.is_some() {
+                        return Err(FromValueError::DuplicateInstanceVariable { name: key.into() });
+                    }
+
+                    width_field = Some(FromValue::from_value(arena, *value, visited)?);
+                }
+                ENCOUNTER_LIST_FIELD => {
+                    if encounter_step_field.is_some() {
+                        return Err(FromValueError::DuplicateInstanceVariable { name: key.into() });
+                    }
+
+                    let encounter_list: &ArrayValue =
+                        FromValue::from_value(arena, *value, visited)?;
+
+                    if !encounter_list.is_empty() {
+                        todo!("ENCOUNTER_LIST");
+                    }
+
+                    encounter_list_field = Some(Vec::new());
+                }
                 _ => {
                     todo!("{:#?}", std::str::from_utf8(key))
                 }
@@ -118,11 +186,41 @@ impl<'a> FromValue<'a> for Map {
         let events = events_field.ok_or(FromValueError::MissingInstanceVariable {
             name: EVENTS_FIELD.into(),
         })?;
+        let bgs = bgs_field.ok_or(FromValueError::MissingInstanceVariable {
+            name: BGS_FIELD.into(),
+        })?;
+        let autoplay_bgm = autoplay_bgm_field.ok_or(FromValueError::MissingInstanceVariable {
+            name: AUTOPLAY_BGM_FIELD.into(),
+        })?;
+        let autoplay_bgs = autoplay_bgs_field.ok_or(FromValueError::MissingInstanceVariable {
+            name: AUTOPLAY_BGS_FIELD.into(),
+        })?;
+        let height = height_field.ok_or(FromValueError::MissingInstanceVariable {
+            name: HEIGHT_FIELD.into(),
+        })?;
+        let encounter_step =
+            encounter_step_field.ok_or(FromValueError::MissingInstanceVariable {
+                name: ENCOUNTER_STEP_FIELD.into(),
+            })?;
+        let width = width_field.ok_or(FromValueError::MissingInstanceVariable {
+            name: WIDTH_FIELD.into(),
+        })?;
+        let encounter_list =
+            encounter_list_field.ok_or(FromValueError::MissingInstanceVariable {
+                name: ENCOUNTER_LIST_FIELD.into(),
+            })?;
 
         Ok(Self {
             bgm,
             tileset_id,
             events,
+            bgs,
+            autoplay_bgm,
+            autoplay_bgs,
+            height,
+            encounter_step,
+            width,
+            encounter_list,
         })
     }
 }
