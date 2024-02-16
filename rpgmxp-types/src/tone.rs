0b0016d87f176d6602a6c4227156065b594e4d9d
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2024-02-15 18:49:12 -0800

    Impl FromValue for Tone

--- rpgmxp-types/src/tone.rs
+++ rpgmxp-types/src/tone.rs
@@ -1,9 +1,30 @@
 use ruby_marshal::FromValue;
 use ruby_marshal::FromValueContext;
 use ruby_marshal::FromValueError;
+use ruby_marshal::SymbolValue;
+use ruby_marshal::UserDefinedValue;
 use ruby_marshal::Value;
 
-// pub(crate) const USER_DEFINED_NAME: &[u8] = b"RPG::AudioFile";
+pub(crate) const USER_DEFINED_NAME: &[u8] = b"Tone";
+
+const SIZE: usize = 8 * 4;
+
+#[derive(Debug)]
+pub enum ToneFromValueError {
+    InvalidSize { size: usize },
+}
+
+impl std::fmt::Display for ToneFromValueError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::InvalidSize { size } => {
+                write!(f, "invalid Tone data size of {size}, expected {SIZE}")
+            }
+        }
+    }
+}
+
+impl std::error::Error for ToneFromValueError {}
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Tone {
@@ -14,7 +35,39 @@ pub struct Tone {
 }
 
 impl<'a> FromValue<'a> for Tone {
-    fn from_value(_ctx: &FromValueContext, _value: &Value) -> Result<Self, FromValueError> {
-        todo!()
+    fn from_value(ctx: &FromValueContext, value: &Value) -> Result<Self, FromValueError> {
+        let user_defined: &UserDefinedValue = FromValue::from_value(ctx, value)?;
+        let name = user_defined.name();
+        let name: &SymbolValue = ctx.from_value(name.into())?;
+        let name = name.value();
+        if name != USER_DEFINED_NAME {
+            return Err(FromValueError::UnexpectedObjectName { name: name.into() });
+        }
+
+        let value = user_defined.value();
+
+        let value_len = value.len();
+        if value_len != SIZE {
+            return Err(FromValueError::new_other(ToneFromValueError::InvalidSize {
+                size: value_len,
+            }));
+        }
+
+        let (red, value) = value.split_at(8);
+        let (green, value) = value.split_at(8);
+        let (blue, value) = value.split_at(8);
+        let (gray, _value) = value.split_at(8);
+
+        let red = f64::from_le_bytes(red.try_into().unwrap());
+        let green = f64::from_le_bytes(green.try_into().unwrap());
+        let blue = f64::from_le_bytes(blue.try_into().unwrap());
+        let gray = f64::from_le_bytes(gray.try_into().unwrap());
+
+        Ok(Self {
+            red,
+            green,
+            blue,
+            gray,
+        })
     }
 }
