2bc8e86e15109a8c348472ab35c75625f42df845
nathaniel-daniel <80441888+nathaniel-daniel@users.noreply.github.com>
2024-02-17 18:55:26 -0800

    Add CompressedScript Type (#8)
    * Add compressed script type
    
    * Make compressed script public
    
    * Make type mods public

--- rpgmxp-types/src/script.rs
+++ rpgmxp-types/src/script.rs
@@ -1,14 +1,18 @@
+use flate2::bufread::ZlibDecoder;
+use ruby_marshal::ArrayValue;
 use ruby_marshal::FromValue;
 use ruby_marshal::FromValueContext;
+use ruby_marshal::FromValueError;
 use ruby_marshal::IntoValue;
 use ruby_marshal::StringValue;
 use ruby_marshal::Value;
 use ruby_marshal::ValueArena;
 use ruby_marshal::ValueHandle;
+use std::io::Read;
 
 /// Invalid script ruby data
 #[derive(Debug)]
-pub enum InvalidScriptError {
+pub enum ScriptFromValueError {
     /// The array len was invalid.
     InvalidArrayLen { len: usize },
 
@@ -16,7 +20,7 @@ pub enum InvalidScriptError {
     InvalidName { error: std::str::Utf8Error },
 }
 
-impl std::fmt::Display for InvalidScriptError {
+impl std::fmt::Display for ScriptFromValueError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::InvalidArrayLen { len } => {
@@ -29,7 +33,7 @@ impl std::fmt::Display for InvalidScriptError {
     }
 }
 
-impl std::error::Error for InvalidScriptError {
+impl std::error::Error for ScriptFromValueError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
             Self::InvalidName { error } => Some(error),
@@ -38,17 +42,17 @@ impl std::error::Error for InvalidScriptError {
     }
 }
 
-impl From<InvalidScriptError> for ruby_marshal::FromValueError {
-    fn from(error: InvalidScriptError) -> Self {
-        ruby_marshal::FromValueError::Other {
+impl From<ScriptFromValueError> for FromValueError {
+    fn from(error: ScriptFromValueError) -> Self {
+        FromValueError::Other {
             error: error.into(),
         }
     }
 }
 
-/// A Script
-#[derive(Debug, Clone)]
-pub struct Script {
+/// A compressed script
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+pub struct CompressedScript {
     /// A randomly-generated script id.
     ///
     /// This should always be positive.
@@ -61,24 +65,21 @@ pub struct Script {
     pub data: Vec<u8>,
 }
 
-impl<'a> FromValue<'a> for Script {
-    fn from_value(
-        ctx: &FromValueContext<'a>,
-        value: &Value,
-    ) -> Result<Self, ruby_marshal::FromValueError> {
-        let script: &ruby_marshal::ArrayValue = FromValue::from_value(ctx, value)?;
+impl<'a> FromValue<'a> for CompressedScript {
+    fn from_value(ctx: &FromValueContext<'a>, value: &Value) -> Result<Self, FromValueError> {
+        let script: &ArrayValue = FromValue::from_value(ctx, value)?;
         let script = script.value();
 
         let array_len = script.len();
         if array_len != 3 {
-            return Err(InvalidScriptError::InvalidArrayLen { len: array_len }.into());
+            return Err(ScriptFromValueError::InvalidArrayLen { len: array_len }.into());
         }
 
         let id: i32 = ctx.from_value(script[0])?;
 
         let name: &StringValue = ctx.from_value(script[1])?;
         let name = std::str::from_utf8(name.value())
-            .map_err(|error| InvalidScriptError::InvalidName { error })?;
+            .map_err(|error| ScriptFromValueError::InvalidName { error })?;
 
         let data: &StringValue = ctx.from_value(script[2])?;
         let data = data.value();
@@ -91,7 +92,7 @@ impl<'a> FromValue<'a> for Script {
     }
 }
 
-impl IntoValue for Script {
+impl IntoValue for CompressedScript {
     fn into_value(
         self,
         arena: &mut ValueArena,
@@ -103,5 +104,38 @@ impl IntoValue for Script {
         let array = arena.create_array(vec![id, name, data]);
 
         Ok(array.into())
+    }
+}
+
+/// A decompressed script
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+pub struct Script {
+    /// A randomly-generated script id.
+    ///
+    /// This should always be positive.
+    pub id: i32,
+
+    /// The name of the script.
+    pub name: String,
+
+    /// The script data.
+    pub data: String,
+}
+
+impl<'a> FromValue<'a> for Script {
+    fn from_value(ctx: &FromValueContext<'a>, value: &Value) -> Result<Self, FromValueError> {
+        let script: CompressedScript = FromValue::from_value(ctx, value)?;
+
+        let mut decoder = ZlibDecoder::new(&*script.data);
+        let mut data = String::new();
+        decoder
+            .read_to_string(&mut data)
+            .map_err(FromValueError::new_other)?;
+
+        Ok(Self {
+            id: script.id,
+            name: script.name,
+            data,
+        })
     }
 }
