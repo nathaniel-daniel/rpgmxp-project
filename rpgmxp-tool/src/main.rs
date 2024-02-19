e7d36a205e99bd68de1a7a637e1ba58655bbae64
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2024-02-18 19:07:12 -0800

    Begin work on rpgmxp-tool

---
+++ rpgmxp-tool/src/main.rs
@@ -0,0 +1,23 @@
+mod commands;
+
+#[derive(Debug, argh::FromArgs)]
+#[argh(description = "a cli tool to interact with rpgmaker xp games")]
+struct Options {
+    #[argh(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(Debug, argh::FromArgs)]
+#[argh(subcommand)]
+enum Subcommand {
+    Unpack(self::commands::unpack::Options),
+}
+
+fn main() -> anyhow::Result<()> {
+    let options: Options = argh::from_env();
+    match options.subcommand {
+        Subcommand::Unpack(options) => self::commands::unpack::exec(options)?,
+    }
+
+    Ok(())
+}
