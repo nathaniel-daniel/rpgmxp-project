a432d9a7865075f5826d4855c42fed0e04ca147f
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2024-09-08 19:31:19 -0700

    Move Tone to common crate

--- rpgm-common-types/src/lib.rs
+++ rpgm-common-types/src/lib.rs
@@ -3,12 +3,16 @@ pub mod map_info;
 pub mod script;
 pub mod system_test_battler;
 pub mod table;
+pub mod tone;
 pub mod util;
 
-pub use self::color::*;
+pub use self::color::Color;
+pub use self::color::ColorFromValueError;
 pub use self::map_info::*;
 pub use self::script::*;
 pub use self::system_test_battler::*;
 pub use self::table::*;
+pub use self::tone::Tone;
+pub use self::tone::ToneFromValueError;
 pub use self::util::ruby_string2string;
 pub use self::util::string2ruby_string;
