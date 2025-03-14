67fe47caaca44f746253b65fea0a8010125eb7fe
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2025-03-14 06:14:49 +0000

    deploy: 4e53055403fadebe99f77f6509fbc7e6ee127f0e

---
+++ trait.impl/argh/trait.TopLevelCommand.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rpgm_tool",[["impl TopLevelCommand for <a class=\"struct\" href=\"rpgm_tool/struct.Options.html\" title=\"struct rpgm_tool::Options\">Options</a>"]]],["rpgmxp_project",[["impl TopLevelCommand for <a class=\"struct\" href=\"rpgmxp_project/struct.Options.html\" title=\"struct rpgmxp_project::Options\">Options</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[151,167]}
